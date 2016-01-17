ba5bcb6ab0609f31f23f197bda282d3bb32cd143
ivan-kostko <kostko_i@yahoo.com>
2016-01-17 08:47:18 +0100

    commonInterfaces

---
+++ Libs/src/CommonInterfaces/CommonInterfaces.go
@@ -0,0 +1,25 @@
+// CommonInterfaces project CommonInterfaces.go
+// The package contains commenly used interfaces
+package commonInterfaces
+
+import (
+	. "customErrors"
+)
+
+// Interface represents Initialize methood
+type Initializer interface {
+	// The method sets up instance and returns error if instance won't be initialized
+	Initialize() Error
+}
+
+// Interface represents MustInitialize methood
+type Initializer interface {
+	// The method sets up instance and panics if instance won't be initialized
+	MustInitialize()
+}
+
+// Interface represents Dispose method
+type Disposer interface {
+	// The method "cleans" all internal referencies to let current instance to be garbage collected
+	Dispose()
+}
