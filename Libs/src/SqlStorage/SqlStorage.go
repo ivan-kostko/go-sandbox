830ff34a8e417e67f4331ddcef39fbe4ba7df1a0
ivan-kostko <kostko_i@yahoo.com>
2016-01-29 08:59:54 +0100

    ISqlStorage explicit definition

--- Libs/src/SqlStorage/SqlStorage.go
+++ Libs/src/SqlStorage/SqlStorage.go
@@ -3,6 +3,7 @@ package SqlStorage
 
 import (
 	"fmt"
+	"reflect"
 
 	"Logger"
 	"SqlStorage/SqlDialects"
@@ -41,6 +42,27 @@ type ISqlStorage interface {
 	Initializer
 	MustInitializer
 	Disposer
+
+	// Fills up model m with data from the first entry at database by:
+	// if atleast one of PK field is not nil - then by PK fields
+	// else by BK fields even if they are all nill
+	// NB: the real type of m should be registered upon the operation. Otherwise returns Notsupported *Error
+	Get(m interface{}) *Error
+
+	// Stores data from model m into database. Does not resolve any values by its own
+	// NB: the real type of m should be registered upon the operation. Otherwise returns Notsupported *Error
+	Put(m interface{}) *Error
+
+	// Tries to store data from model m into database
+	// If there are fields which are resolved on DB level(sequence, default values or calculated on DB level)
+	// fills up model with this values
+	Resolve(m interface{}) *Error
+
+	// Removes entry from database by:
+	// if atleast one of PK field is not nil - then by PK fields
+	// else by BK fields even if they are all nill
+	// NB: the real type of m should be registered upon the operation. Otherwise returns Notsupported *Error
+	Del(m interface{}) *Error
 }
 
 // Generic (I)SqlStorage factory.
